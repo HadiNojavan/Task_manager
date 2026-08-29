2b0a4f44d03bd524c7cf91109cf8ee28f695228c
hadi <hadinojvan6@gmail.com>
2026-08-29 16:24:45 +0330

    add register request

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,10 +1,30 @@
 <?php
 
+namespace src\Models;
+
+use src\Core\Database;
+
+
 class User{
     protected $database;
     
     public function __construct(Database $database) {
         $this->database=$database;
     }
-    
+
+    public function create($username, $password){//create new user in database
+
+    return $this->database->query( "INSERT INTO users (username, password)VALUES (?, ?) RETURNING *",
+            [$username, $password])
+        ->fetch();
 }
+
+    public function findByUsername($username){//to check that our new username exits in database or not 
+
+        return $this->database
+            ->query('SELECT username FROM users WHERE username=?',[$username])
+            ->fetch();
+        
+    }
+    }
+    
