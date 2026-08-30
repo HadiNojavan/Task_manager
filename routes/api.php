7b0e2912daa512a537eee703843b11866e4a4c2a
hadi <hadinojvan6@gmail.com>
2026-08-30 13:50:57 +0330

    add basic AUTH but still need to work on it

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use src\Controllers\Login;
 
 $router->get('/api/tasks', [Tasks::class, 'index']); // here we get all tasks from database that method in controllers is index to get all info 
 $router->get('/api/tasks/{id}', [Tasks::class, 'show']);//here we get only one task by id 
-$router->post('/api/tasks', [Tasks::class, 'store']);//here i create new task 
+$router->post('/api/tasks', [Tasks::class, 'store'])->only('auth');//here i create new task 
 $router->patch('/api/tasks/{id}', [Tasks::class, 'update']);//this update out task by id
 $router->delete('/api/tasks/{id}', [Tasks::class, 'destroy']);//here we can delete one task by id 
 
---
+++ src/Middleware/TokenAuth.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace src\Middleware;
+
+use src\Core\Database;
+use src\Models\User;
+
+class TokenAuth {
+    private $db;
+
+    public function __construct(Database $db) {
+        $this->db = $db;
+    }
+
+    public function handle(){
+        $headers = getallheaders();// "Bearer dkfjf3xff......"
+        $authHeader = $headers['Authorization'] ?? '';
+
+        if (!str_starts_with($authHeader, 'Bearer ')) {
+            // there is no token in header 
+            abort(401, 'Authorization token required');
+        }
+
+        //we must remove Bearer part 
+        $token = substr($authHeader, 7);
+        
+        $user= new User($this->db);
+        $search_for_token=$user->findtoken($token);
+        
+        //if return false means we can not find given token in table
+        if(!$search_for_token){
+            abort(401, 'Invalid token');
+        }
+
+        //if we reach here we find the token and we can safely go to controller
+        
+    }
+
+
+
+
+
+
+
+}
